0672b9a2a97ee761b18b5c3e563a3231e09a356e
fisherdeng <fisherpeng.deng@gmail.com>
2019-10-23 16:53:41 +0800

    微信小程序的客户端程序中“通过canvas元素绘制N元（N种颜色）统计饼图”方法。
    Signed-off-by: fisherdeng <fisherpeng.deng@gmail.com>

--- code/client/models/modelLoad.js
+++ code/client/models/modelLoad.js
@@ -1,7 +1,7 @@
 //modelLoad.js
 var openId = null
 var nickName = null
-var result = null//['', '', '', '', '']
+var result = null //['', '', '', '', '']
 var results = new Array()
 var recordPosition = 0
 var tableId = 0
@@ -18,87 +18,121 @@ Page({
     //texts: 'init01 data',
     //array: [{ text: 'init02 data' }],
     //object: {
-      //texts: 'init03 data'
+    //texts: 'init03 data'
     //},
     //inputTester:"点我啊" 
-    tables: 
-    [
-      { id: 0, name: '银行账户', labels: ['ID', '公司代码', '开户银行', '银行账号', '币种代码', '备注信息'] }, 
-      { id: 1, name: '公司信息', labels: ['ID', '公司代码', '公司简称', '公司全称', '商业类型代码', '电话', '传真', '网址', '注册地址', '公司税号', '采购部/销售部联系人地址', '采购部/销售部联系人', '采购部/销售部联系人手机', '采购部/销售部联系人邮箱', '财务部联系人地址', '财务部联系人', '财务部联系人手机', '财务部联系人邮箱', '发货金额总计', '付款金额总计', '余款/欠款总计', '未开票金额总计', '币种'] }, 
-      { id: 2, name: '收货地址', labels: ['ID', '公司代码', '收货地址', '收货人员', '联系方式'] }, 
-      { id: 3, name: '合同信息', labels: ['ID', '合同编号', '合同日期', '公司代码', '产品代码', '合同数量', '合同价格', '合同已执行数量', '备注'] },
-      { id: 4, name: '收支方向', labels: ['ID', '方向代码', '方向名称'] },
-      { id: 5, name: '发票信息', labels: ['ID', '发票号码', '开票日期', '发票金额', '方向代码', '公司代码', '备注'] },
-      { id: 6, name: '付款类型', labels: ['ID', '类型代码', '类型名称'] },
-      { id: 7, name: '付款明细', labels: ['ID', '付款日期', '收支方向代码', '公司代码', '付款类型代码', '付款金额', '备注'] },
-      { id: 8, name: '产品名称', labels: ['ID', '产品代码', '产品名称', '备注'] }       
+    tables: [{
+        id: 0,
+        name: '银行账户',
+        labels: ['ID', '公司代码', '开户银行', '银行账号', '币种代码', '备注信息']
+      },
+      {
+        id: 1,
+        name: '公司信息',
+        labels: ['ID', '公司代码', '公司简称', '公司全称', '商业类型代码', '电话', '传真', '网址', '注册地址', '公司税号', '采购部/销售部联系人地址', '采购部/销售部联系人', '采购部/销售部联系人手机', '采购部/销售部联系人邮箱', '财务部联系人地址', '财务部联系人', '财务部联系人手机', '财务部联系人邮箱', '发货金额总计', '付款金额总计', '余款/欠款总计', '未开票金额总计', '币种']
+      },
+      {
+        id: 2,
+        name: '收货地址',
+        labels: ['ID', '公司代码', '收货地址', '收货人员', '联系方式']
+      },
+      {
+        id: 3,
+        name: '合同信息',
+        labels: ['ID', '合同编号', '合同日期', '公司代码', '产品代码', '合同数量', '合同价格', '合同已执行数量', '备注']
+      },
+      {
+        id: 4,
+        name: '收支方向',
+        labels: ['ID', '方向代码', '方向名称']
+      },
+      {
+        id: 5,
+        name: '发票信息',
+        labels: ['ID', '发票号码', '开票日期', '发票金额', '方向代码', '公司代码', '备注']
+      },
+      {
+        id: 6,
+        name: '付款类型',
+        labels: ['ID', '类型代码', '类型名称']
+      },
+      {
+        id: 7,
+        name: '付款明细',
+        labels: ['ID', '付款日期', '收支方向代码', '公司代码', '付款类型代码', '付款金额', '备注']
+      },
+      {
+        id: 8,
+        name: '产品名称',
+        labels: ['ID', '产品代码', '产品名称', '备注']
+      }
     ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    util.loadOpenId(openId,nickName)
+  onLoad: function(options) {
+    util.loadOpenId(openId, nickName)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     //wx.navigateTo({
-      //url: 'pages/index/index',
+    //url: 'pages/index/index',
     //})
   },
 
-  onInputConfirm:function(e){
+  onInputConfirm: function(e) {
     //inputValue:'hahahha'
     //this.setData
   },
 
-  onSubmit:function(e){
+  onSubmit: function(e) {
     /*var formData=e.detail.value
     if(formData.truckID!="")
     {
@@ -209,160 +243,158 @@ Page({
       }
     })
     */
-    
-    
+
+
     //buttonType ="primary"
     //this.setData
   },
 
-  changeText: function () {
+  changeText: function() {
     // this.data.text = 'changed data' // bad, it can not work 
     this.setData({
       texts: 'fuck u'
     })
   },
-  changeItemInArray: function () {
+  changeItemInArray: function() {
     // you can use this way to modify a danamic data path 
     this.setData({
       'array[0].text': 'changed data'
     })
   },
-  changeItemInObject: function () {
+  changeItemInObject: function() {
     this.setData({
       'object.texts': 'changed??? data'
     });
   },
-  addNewField: function () {
+  addNewField: function() {
     this.setData({
       'newField': 'new data',
-      'newButton.text':'new data----------'
+      'newButton.text': 'new data----------'
     })
-  }, 
+  },
 
   //选择对应的基础数据表
-  bindPickerChange: function (e) {
+  bindPickerChange: function(e) {
     //var that=this
     console.log('picker发送选择改变，携带值为', e.detail.value)
     //result=tables[index].texts
-    this.setData
-    (
-      {
-        index: e.detail.value,
-        result: []
-      }
-    )
-    tableId=e.detail.value
-    results=[]
-    recordPosition=0
+    this.setData({
+      index: e.detail.value,
+      result: []
+    })
+    tableId = e.detail.value
+    results = []
+    recordPosition = 0
     //console.log(tables[counter].labels)
     //util.getFullTableDataFromODataService('bankaccouts', nickName,result,results,recordPosition,that)
   },
 
   //下一条记录
-  bindNextRecord:function(e)
-  {
-    if(recordPosition<results.length-1)
-    {
+  bindNextRecord: function(e) {
+    if (recordPosition < results.length - 1) {
       recordPosition++
     }
-    if (result == null) 
-    {
+    if (result == null) {
       var that = this
-      if (tableId == 0)
-      {
+      if (tableId == 0) {
         util.getFullTableDataFromODataService('bankaccouts', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 1)
-      {
+      if (tableId == 1) {
         util.getFullTableDataFromODataService('companies', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 2) 
-      {
+      if (tableId == 2) {
         util.getFullTableDataFromODataService('companydeliveryaddresses', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 3) 
-      {
+      if (tableId == 3) {
         util.getFullTableDataFromODataService('contracts', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 4) 
-      {
+      if (tableId == 4) {
         util.getFullTableDataFromODataService('directions', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 5) 
-      {
+      if (tableId == 5) {
         util.getFullTableDataFromODataService('invoices', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 6) 
-      {
+      if (tableId == 6) {
         util.getFullTableDataFromODataService('paymenttypes', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 7) 
-      {
+      if (tableId == 7) {
         util.getFullTableDataFromODataService('payments', nickName, result, results, recordPosition, that)
       }
-      if (tableId == 8) 
-      {
+      if (tableId == 8) {
         util.getFullTableDataFromODataService('products', nickName, result, results, recordPosition, that)
       }
     }
-    console.log('recordPosition is'+recordPosition)
+    console.log('recordPosition is' + recordPosition)
     this.setData({
-      result:results[recordPosition]
+      result: results[recordPosition]
     })
   },
 
   //上一条记录
-  bindLastRecord:function(e)
-  {
-    if(recordPosition>0)
-    {
+  bindLastRecord: function(e) {
+    if (recordPosition > 0) {
       recordPosition--
-    }
-    else
-    {
-      if (result == null)
-      { var that=this
-        if (tableId == 0) 
-        {
+    } else {
+      if (result == null) {
+        var that = this
+        if (tableId == 0) {
           util.getFullTableDataFromODataService('bankaccouts', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 1) 
-        {
+        if (tableId == 1) {
           util.getFullTableDataFromODataService('companies', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 2) 
-        {
+        if (tableId == 2) {
           util.getFullTableDataFromODataService('companydeliveryaddresses', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 3) 
-        {
+        if (tableId == 3) {
           util.getFullTableDataFromODataService('contracts', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 4) 
-        {
+        if (tableId == 4) {
           util.getFullTableDataFromODataService('directions', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 5) 
-        {
+        if (tableId == 5) {
           util.getFullTableDataFromODataService('invoices', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 6) 
-        {
+        if (tableId == 6) {
           util.getFullTableDataFromODataService('paymenttypes', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 7) 
-        {
+        if (tableId == 7) {
           util.getFullTableDataFromODataService('payments', nickName, result, results, recordPosition, that)
         }
-        if (tableId == 8) 
-        {
+        if (tableId == 8) {
           util.getFullTableDataFromODataService('products', nickName, result, results, recordPosition, that)
         }
       }
     }
-    console.log('recordPosition is'+recordPosition)
+    console.log('recordPosition is' + recordPosition)
     this.setData({
-      result:results[recordPosition]
+      result: results[recordPosition]
     })
+    //选择“公司信息”基础表时，绘制N元统计饼图
+    if (tableId == 1) {
+      var invoicedAmount = parseFloat(results[recordPosition][18]) - parseFloat(results[recordPosition][21]) //此处“已发货金额”和“未开发票金额”要先转换成数字类型，否则可能会出现字符串类型参与之后的计算而引发错误
+      invoicedAmount = parseFloat(invoicedAmount.toFixed(2))
+      var uninvoicedAmount = parseFloat(results[recordPosition][21]) //此处“未开发票金额”要先转换成数字类型，否则可能会出现字符串类型参与之后的计算而引发错误
+      uninvoicedAmount = parseFloat(uninvoicedAmount.toFixed(2))
+      var datas = [invoicedAmount, uninvoicedAmount]
+      console.log(datas)
+      //console.log(datas[0]+datas[1])
+      //console.log(results[recordPosition][18])
+      var labels = ['已开发票金额(元)', '未开发票金额(元)']
+      var colors = ['green', 'orange']
+      util.drawArc('canvasInvoicedAmount', 61, 100, 60, datas, colors, labels)
+      labels = ['已付款金额(元)', '欠款金额(元)']
+      var payedAmount = parseFloat(results[recordPosition][19]) //此处“已付款金额”要先转换成数字类型，否则可能会出现字符串类型参与之后的计算而引发错误
+      var unPayedAmount = parseFloat(results[recordPosition][18]) - payedAmount
+      payedAmount = parseFloat(payedAmount.toFixed(2))
+      unPayedAmount = parseFloat(unPayedAmount.toFixed(2))
+      if (unPayedAmount < 0) {
+        unPayedAmount = 0 - unPayedAmount
+        labels[1] = '预付款金额'
+        colors[1] = 'blue'
+      }
+      datas = [payedAmount, unPayedAmount]
+      util.drawArc('canvasPayedAmount', 61, 100, 60, datas, colors, labels)
+    }
   }
 })
--- code/client/utils/util.js
+++ code/client/utils/util.js
@@ -17,22 +17,22 @@ function formatNumber(n) {
 }
 
 //获取当前微信小程序用户的openId和昵称，存入页面变量中
-function loadOpenId(openId,nickName){
+function loadOpenId(openId, nickName) {
   if (openId == null) {
     var loginCode
     /*第一次调用wx.login()，以获取openId和nickName*/
     wx.login({
-      success: function (res) {
+      success: function(res) {
         /*以下为向微信小程序登录服务器换取登录令牌*/
         loginCode = res.code
         wx.request({
           url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx7e6c11974fbb3699&secret=a2af134685148f465721879f6ceab094&js_code=' + loginCode + '&grant_type=authorization_code',
-          success: function (res) {
+          success: function(res) {
             console.log("this is sessionKey: " + res.data["session_key"])
             openId = res.data.openid
             console.log("this is openId: " + openId)
           },
-          fail: function (res) {
+          fail: function(res) {
             console.log("换取登录令牌失败")
           }
         })
@@ -43,7 +43,7 @@ function loadOpenId(openId,nickName){
         wx.getUserInfo({
           openIdList: ['selfOpenId'],
           lang: 'zh_CN',
-          success: function (res) {
+          success: function(res) {
             nickName = res.userInfo.nickName
             console.log('this is the nickName: ' + nickName)
           }
@@ -55,89 +55,116 @@ function loadOpenId(openId,nickName){
 }
 
 //根据基础数据表名称、微信昵称装载对应的基础数据表数据进results中，并将第1条记录装载进reslut中在页面中显示，that用于异步回调setData()方法绑定刷新数据
-function getFullTableDataFromODataService(serviceName,nickName,result,results,recordPosition,that){
-  if (results.length == 0) 
-  {
+function getFullTableDataFromODataService(serviceName, nickName, result, results, recordPosition, that) {
+  if (results.length == 0) {
     wx.login({
-      success:function(res)
-      {
+      success: function(res) {
         //微信的login方法返回“成功”时获取为第三方服务器ODataService准备的loginCode
-        var loginCode=res.code
-        console.log('this is loginCode for ODataService:'+loginCode)
+        var loginCode = res.code
+        console.log('this is loginCode for ODataService:' + loginCode)
         //从微信小程序端向第三方服务器的ODataService发起HTTP的REQUEST请求
         console.log(results.length)
         //if (results.length==0){
-          //serviceName+='(\''+recordIds[recordPosition]+'\')'
-          wx.request
-          (
-            {
-                url: 'http://localhost:57499/odata/' + serviceName + '?loginCode=' + loginCode + '&nickName=' + nickName,
-                method: 'GET',
-                success: function (res) 
-                {
-                  console.log(res)
-                  console.log(res.data.value[0].CompanyCode)
-                  console.log(res.data.value.length)//非常重要，这里返回了成功返回的记录条数
-                  //recordPosition=0
-                  //result=res.data.value[0].CompanyCode
-                  //results = new Array()
-                  for (var index = 0; index < res.data.value.length; index++) 
-                  {
-                    var tempResult=new Array()
-                    if (serviceName =='bankaccouts')
-                    {
-                      tempResult = [res.data.value[index].ID,res.data.value[index].CompanyCode, res.data.value[index].BankName, res.data.value[index].BankAccount, res.data.value[index].CurrencyCode, res.data.value[index].Note]
-                    }
-                    if (serviceName == 'companies')
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].CompanyCode, res.data.value[index].ShortName, res.data.value[index].FullName, res.data.value[index].BusinessDirectionCode, res.data.value[index].PhoneNumber, res.data.value[index].FaxNumber, res.data.value[index].Website, res.data.value[index].Address, res.data.value[index].TaxNumber, res.data.value[index].SalesContactAddress, res.data.value[index].SalesContact, res.data.value[index].SalesContactMobile, res.data.value[index].SalesContactEmail, res.data.value[index].AccountingContactAddress, res.data.value[index].AccountingContact, res.data.value[index].AccountingContactMobile, res.data.value[index].AccountingContactEmail, res.data.value[index].TotalDeliveryAmount, res.data.value[index].TotalPaymentAmount, res.data.value[index].TotalBalanceAmount, res.data.value[index].TotalUninvoiceAmount, res.data.value[index].CurrencyCode]
-                    }
-                    if (serviceName == 'companydeliveryaddresses') 
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].CompanyCode, res.data.value[index].CargoDeliveryAddress, res.data.value[index].CargoDeliveryContact, res.data.value[index].CargoDeliveryContactMobile]
-                    }
-                    if (serviceName == 'contracts') 
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].ContractCode, res.data.value[index].ContractDate, res.data.value[index].CompanyCode, res.data.value[index].ProductCode, res.data.value[index].ContractAmount, res.data.value[index].ContractPrice, res.data.value[index].ContractExcutedAmount, res.data.value[index].Note]
-                    }
-                    if (serviceName == 'directions') 
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].DirectionCode, res.data.value[index].DirectionDesc]
-                    }
-                    if (serviceName == 'invoices') 
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].InvoiceCode, res.data.value[index].InvoiceDate, res.data.value[index].InvoiceAmount, res.data.value[index].InvoiceDirectionCode, res.data.value[index].CompanyCode, res.data.value[index].Note]
-                    }
-                    if (serviceName == 'paymenttypes') 
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].PaymentTypeCode, res.data.value[index].PaymentTypeDesc]
-                    }
-                    if (serviceName == 'payments') 
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].PaymentDate, res.data.value[index].PaymentDirectionCode, res.data.value[index].CompanyCode, res.data.value[index].PaymentTypeCode, res.data.value[index].PaymentAmount, res.data.value[index].Note]
-                    }
-                    if (serviceName == 'products') 
-                    {
-                      tempResult = [res.data.value[index].ID, res.data.value[index].ProductCode, res.data.value[index].ProductName, res.data.value[index].Note]
-                    }
-                    results[index] = tempResult
-                  }
-                  that.setData({
-                    recordPosition: 0,
-                    result: results[recordPosition]
-                    //recordIds:res.data.value.count,
-                  })
-                }
+        //serviceName+='(\''+recordIds[recordPosition]+'\')'
+        wx.request({
+          url: 'http://localhost:57499/odata/' + serviceName + '?loginCode=' + loginCode + '&nickName=' + nickName,
+          method: 'GET',
+          success: function(res) {
+            console.log(res)
+            console.log(res.data.value[0].CompanyCode)
+            console.log(res.data.value.length) //非常重要，这里返回了成功返回的记录条数
+            //recordPosition=0
+            //result=res.data.value[0].CompanyCode
+            //results = new Array()
+            for (var index = 0; index < res.data.value.length; index++) {
+              var tempResult = new Array()
+              if (serviceName == 'bankaccouts') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].CompanyCode, res.data.value[index].BankName, res.data.value[index].BankAccount, res.data.value[index].CurrencyCode, res.data.value[index].Note]
+              }
+              if (serviceName == 'companies') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].CompanyCode, res.data.value[index].ShortName, res.data.value[index].FullName, res.data.value[index].BusinessDirectionCode, res.data.value[index].PhoneNumber, res.data.value[index].FaxNumber, res.data.value[index].Website, res.data.value[index].Address, res.data.value[index].TaxNumber, res.data.value[index].SalesContactAddress, res.data.value[index].SalesContact, res.data.value[index].SalesContactMobile, res.data.value[index].SalesContactEmail, res.data.value[index].AccountingContactAddress, res.data.value[index].AccountingContact, res.data.value[index].AccountingContactMobile, res.data.value[index].AccountingContactEmail, res.data.value[index].TotalDeliveryAmount, res.data.value[index].TotalPaymentAmount, res.data.value[index].TotalBalanceAmount, res.data.value[index].TotalUninvoiceAmount, res.data.value[index].CurrencyCode]
+              }
+              if (serviceName == 'companydeliveryaddresses') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].CompanyCode, res.data.value[index].CargoDeliveryAddress, res.data.value[index].CargoDeliveryContact, res.data.value[index].CargoDeliveryContactMobile]
+              }
+              if (serviceName == 'contracts') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].ContractCode, res.data.value[index].ContractDate, res.data.value[index].CompanyCode, res.data.value[index].ProductCode, res.data.value[index].ContractAmount, res.data.value[index].ContractPrice, res.data.value[index].ContractExcutedAmount, res.data.value[index].Note]
+              }
+              if (serviceName == 'directions') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].DirectionCode, res.data.value[index].DirectionDesc]
+              }
+              if (serviceName == 'invoices') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].InvoiceCode, res.data.value[index].InvoiceDate, res.data.value[index].InvoiceAmount, res.data.value[index].InvoiceDirectionCode, res.data.value[index].CompanyCode, res.data.value[index].Note]
+              }
+              if (serviceName == 'paymenttypes') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].PaymentTypeCode, res.data.value[index].PaymentTypeDesc]
+              }
+              if (serviceName == 'payments') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].PaymentDate, res.data.value[index].PaymentDirectionCode, res.data.value[index].CompanyCode, res.data.value[index].PaymentTypeCode, res.data.value[index].PaymentAmount, res.data.value[index].Note]
+              }
+              if (serviceName == 'products') {
+                tempResult = [res.data.value[index].ID, res.data.value[index].ProductCode, res.data.value[index].ProductName, res.data.value[index].Note]
+              }
+              results[index] = tempResult
             }
-          )
+            that.setData({
+              recordPosition: 0,
+              result: results[recordPosition]
+              //recordIds:res.data.value.count,
+            })
+          }
+        })
         //}  
       }
     })
   }
 }
 
+//通过canvas元素绘制N元（N种颜色）统计饼图
+function drawArc(canvasID, x, y, radius, datas, colors, lables) {
+  //var canvas=document.getElementById("myCanvas")
+  //var context=canvas.getContext("2d")
+  var angles = 0
+  var context = wx.createCanvasContext(canvasID)
+  //context.fillStyle = "#FF00FF"
+  //context.setFillStyle('#FFA500')
+  for (var index = 0; index < datas.length; index++) {
+    angles += datas[index]
+  }
+  console.log('the angles is' + angles)
+  var startAngle = 0
+  var endAngle = 0
+  var yLabel = 0
+  for (var index = 0; index < datas.length; index++) {
+    //context.setStrokeStyle('#FFA500')
+    endAngle = datas[index] / angles * 2 * Math.PI + startAngle
+    console.log('the endAngle is' + endAngle)
+    context.beginPath() //“起始一条路径，或重置当前路径”。
+    context.setFillStyle(colors[index])
+    context.arc(x, y, radius, startAngle, endAngle, false) //“创建弧/曲线（用于创建圆形或部分圆）”。此处参数“boolean counterclockwise 弧度的方向是否是逆时针”貌似无效。
+    context.lineTo(x, y) //“添加一个新点，然后在画布中创建从该点到最后指定点的线条”。非常重要的一句，缺失则饼图颜色范围残缺不全
+    //context.arc(x, y, radius, startAngle, endAngle)
+    //context.arc(x, y, radius, endAngle, startAngle)
+    context.closePath() //“创建从当前点回到起始点的路径”。
+    //context.stroke() //“绘制已定义的路径”。此处绘制出来的是“圆弧+两端点连线”。
+    context.fill()
+    startAngle = endAngle
+    console.log('the startAngle is' + startAngle)
+    //绘制颜色方块
+    yLabel += 30
+    context.setFillStyle(colors[index])
+    context.fillRect(x + 1.5 * radius, yLabel + 0.5 * y, 15, 15)
+    //绘制文字说明
+    context.setFillStyle('black')
+    context.setFontSize(12)
+    context.fillText(lables[index] + ': ' + datas[index], x + 1.5 * radius + 20, yLabel + 0.5 * y + 15)
+  }
+  context.draw()
+}
+
 module.exports = {
   formatTime: formatTime,
-  loadOpenId:loadOpenId,
-  getFullTableDataFromODataService
+  loadOpenId: loadOpenId,
+  getFullTableDataFromODataService: getFullTableDataFromODataService,
+  drawArc: drawArc
 }
